ab4ad90be1878ed457a3c09f2395a500af4e98a8
ecogle@gmail.com <>
2021-01-08 16:30:24 -0500

    add virtual column

--- ot.sql
+++ ot.sql
@@ -5,6 +5,8 @@ inner join orders USING (order_id)
 INNER JOIN products USING (product_id)
 INNER JOIN product_categories USING (category_id);
 
+
+
 select * from order_stats;
 select * from order_stats
 PIVOT(
@@ -44,3 +46,36 @@ where
     extract (year FROM order_date) = 2017
 group by
     rollup(customer_id);
+    
+    
+    
+select
+    product_id,
+    product_name,
+    list_price,
+    standard_cost
+from
+    products
+where list_price BETWEEN (select avg(list_price)-100 FROM products) AND (select avg(list_price)+100 FROM products);
+
+
+CREATE TABLE parts(
+    part_id INT GENERATED ALWAYS AS IDENTITY,
+    part_name VARCHAR2(50) NOT NULL,
+    capacity INT NOT NULL,
+    cost DEC(15,2) NOT NULL,
+    list_price DEC(15,2) NOT NULL,
+    gross_margin AS ((list_price - cost) / cost),
+    PRIMARY KEY(part_id)
+);
+
+INSERT INTO parts(part_name, capacity, cost, list_price)
+VALUES('G.SKILL TridentZ RGB Series 16GB (2 x 8GB)', 16, 95,105);
+
+INSERT INTO parts(part_name, capacity, cost, list_price)
+VALUES('G.SKILL TridentZ RGB Series 32GB (4x8GB)', 32, 205,220);
+
+INSERT INTO parts(part_name, capacity, cost, list_price)
+VALUES('G.SKILL TridentZ RGB Series 16GB (1 x 8GB)', 8, 50,70);
+
+select * from parts;
